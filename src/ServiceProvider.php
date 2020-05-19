1a711813a94a16c50e98bbe2177a4cb6b4e17bf4
Gail <xxxggggail@gmail.com>
2020-05-19 17:12:13 +0800

    test

---
+++ src/ServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace XXGail\Gvis;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Routing\Router;
+
+class ServiceProvider extends BaseServiceProvider
+{
+
+    /**
+     * 在注册后进行服务的启动。
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * 在容器中注册绑定。
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
